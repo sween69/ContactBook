56f7e3f718fba6ba4bc72b9a886bb2202f4c13aa
Sylvia <sylph_31@hotmail.com>
2023-08-05 23:18:15 +0800

    feat(screens): [WIP] Add ContactInfoScreen

---
+++ src/screens/ContactInfoScreen.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+const ContactInfo: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.toolbar} />
+
+      <View style={styles.profilePicContainer} />
+
+      <View>
+        <Text style={styles.sectionText}>Main Information</Text>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>First Name</Text>
+          <TextInput style={styles.input} />
+        </View>
+
+        <View style={styles.divider} />
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Last Name</Text>
+          <TextInput style={styles.input} />
+        </View>
+      </View>
+
+      <View>
+        <Text style={styles.sectionText}>Sub Information</Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput style={styles.input} placeholder="Enter email" />
+        </View>
+
+        <View style={styles.divider} />
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Phone</Text>
+          <TextInput style={styles.input} placeholder="Enter phone" />
+        </View>
+      </View>
+
+      <View style={styles.divider} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  toolbar: {
+    height: 56,
+    backgroundColor: '#F0F0F0',
+  },
+  profilePicContainer: {
+    width: 150,
+    height: 150,
+    aspectRatio: 1,
+    borderRadius: 75,
+    backgroundColor: '#FF8C00',
+    alignSelf: 'center',
+    marginVertical: 32,
+  },
+  sectionText: {
+    backgroundColor: '#F0F0F0',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 8,
+  },
+  label: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  input: {
+    flex: 4,
+    height: 40,
+    paddingHorizontal: 8,
+    marginRight: 16,
+    borderWidth: 1,
+    borderColor: '#D3D3D3',
+    borderRadius: 4,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: 'darkgray',
+    marginLeft: 16,
+  },
+});
+
+export default ContactInfo;
